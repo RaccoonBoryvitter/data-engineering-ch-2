331ff5dd5089de37360a000ff0e64c616bd05643
Pavel Shcherbatyi <pavel.shcherbatyi@protonmail.com>
2023-11-27 04:31:31 +0200

    #1: Initialization of ready project

---
+++ async.py
@@ -0,0 +1,23 @@
+import asyncio
+from aiohttp import ClientSession
+
+from async_lib import download_resource_async
+
+download_uris = [
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
+]
+output_dir = "downloads"
+
+async def main():
+    async with ClientSession() as session:
+        tasks = [download_resource_async(uri, output_dir, session) for uri in download_uris]
+        await asyncio.gather(*tasks)
+
+if __name__ == "__main__":
+    asyncio.run(main())
